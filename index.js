9864e26295b7f644759d4050257c7a2fc9a9bbd1
mohsin khan <khan.mohsin.00001@gmail.com>
2020-08-27 23:49:50 +0530

    first commit:create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(err){
+if(err){
+    console.log(`error in running of server;${err}`);
+}
+console.log(`server is running on port:${port}`);
+});
